eeb5b4dc51acf028db1bf95c03a637a7adc9861b
LordGee <gordon.johnson@mychaos.co.uk>
2018-02-17 10:55:15 +0000

    Setup initial layout of panels

--- rtp1/rtp1/frm_main.h
+++ rtp1/rtp1/frm_main.h
@@ -9,50 +9,61 @@ namespace rtp1 {
 	using namespace System::Data;
 	using namespace System::Drawing;
 
-	/// <summary>
-	/// Summary for frm_main
-	/// </summary>
 	public ref class frm_main : public System::Windows::Forms::Form
 	{
 	public:
-		frm_main(void)
-		{
-			InitializeComponent();
-			//
-			//TODO: Add the constructor code here
-			//
-		}
+		frm_main(void) { InitializeComponent();	}
 
 	protected:
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		~frm_main()
-		{
-			if (components)
-			{
-				delete components;
-			}
-		}
+		~frm_main()	{ if (components) {	delete components; } }
+	
+	// Custom Components
+	private: System::Windows::Forms::Panel^  pnl_StartOptions;
+	private: System::Windows::Forms::Panel^  pnl_StatInfo;
+	private: System::Windows::Forms::Panel^  pnl_GameCanvas;
+
 
 	private:
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
 		System::ComponentModel::Container ^components;
 
 #pragma region Windows Form Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		void InitializeComponent(void)
-		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"frm_main";
-			this->Padding = System::Windows::Forms::Padding(0);
+		void InitializeComponent(void) {
+			this->pnl_StartOptions = (gcnew System::Windows::Forms::Panel());
+			this->pnl_StatInfo = (gcnew System::Windows::Forms::Panel());
+			this->pnl_GameCanvas = (gcnew System::Windows::Forms::Panel());
+			this->SuspendLayout();
+			// pnl_StartOptions
+			this->pnl_StartOptions->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(224)), static_cast<System::Int32>(static_cast<System::Byte>(192)));
+			this->pnl_StartOptions->Location = System::Drawing::Point(12, 12);
+			this->pnl_StartOptions->Name = L"pnl_StartOptions";
+			this->pnl_StartOptions->Size = System::Drawing::Size(300, 657);
+			this->pnl_StartOptions->TabIndex = 0;
+			// pnl_StatInfo
+			this->pnl_StatInfo->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(224)),
+				static_cast<System::Int32>(static_cast<System::Byte>(192)));
+			this->pnl_StatInfo->Location = System::Drawing::Point(952, 12);
+			this->pnl_StatInfo->Name = L"pnl_StatInfo";
+			this->pnl_StatInfo->Size = System::Drawing::Size(300, 657);
+			this->pnl_StatInfo->TabIndex = 1;
+			// pnl_GameCanvas
+			this->pnl_GameCanvas->BackColor = System::Drawing::Color::Black;
+			this->pnl_GameCanvas->Location = System::Drawing::Point(333, 39);
+			this->pnl_GameCanvas->Name = L"pnl_GameCanvas";
+			this->pnl_GameCanvas->Size = System::Drawing::Size(600, 600);
+			this->pnl_GameCanvas->TabIndex = 2;
+			// frm_main
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1264, 681);
+			this->Controls->Add(this->pnl_GameCanvas);
+			this->Controls->Add(this->pnl_StatInfo);
+			this->Controls->Add(this->pnl_StartOptions);
+			this->DoubleBuffered = true;
+			this->Name = L"frm_main";
+			this->Text = L"RTP Learning";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 	};
